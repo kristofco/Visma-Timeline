cd5546e573c413980490bae6a20f75a3c7ce8773
Yurii <juracherniak@gmail.com>
2022-04-23 21:47:59 +0200

    a

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { TimelineEventComponent } from './timeline-event/timeline-event.component';
 import { EventComponent } from './event/event.component';
+import { ScrollToTopComponent } from './scroll-to-top/scroll-to-top.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { EventComponent } from './event/event.component';
     HeaderComponent,
     TimelineEventComponent,
     EventComponent,
+    ScrollToTopComponent,
     
   ],
   imports: [
