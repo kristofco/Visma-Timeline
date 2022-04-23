6c9d097ed3d065eae67f2652bb0fdcca84a31a89
Yurii <juracherniak@gmail.com>
2022-04-23 17:38:05 +0200

    start slider

--- client/src/app/event/event.component.ts
+++ client/src/app/event/event.component.ts
@@ -3,13 +3,60 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-event',
   templateUrl: './event.component.html',
-  styleUrls: ['./event.component.scss']
+  styleUrls: ['./event.component.scss'],
 })
 export class EventComponent implements OnInit {
+  imageObject: Array<object> = [
+    {
+      image:
+        'https://hackkosice.com/images/events/hk19/arrival_registration_hu9c5b0c793d9bb1618b8c596bf6a17dfb_3125095_640x360_fill_q60_box_smart1.jpg',
+      thumbImage:
+        'https://hackkosice.com/images/events/hk19/arrival_registration_hu9c5b0c793d9bb1618b8c596bf6a17dfb_3125095_640x360_fill_q60_box_smart1.jpg',
+      alt: 'Image 1',
+     // title: 'Image 1',
+    },
+    {
+      image:
+        'https://hackkosice.com/images/events/hk19/arrival_registration_hu9c5b0c793d9bb1618b8c596bf6a17dfb_3125095_640x360_fill_q60_box_smart1.jpg',
+      thumbImage:
+        'https://hackkosice.com/images/events/hk19/arrival_registration_hu9c5b0c793d9bb1618b8c596bf6a17dfb_3125095_640x360_fill_q60_box_smart1.jpg',
+    //  title: 'Image 2',
+      alt: 'Image 2',
+    },
+    {
+      image:
+        'https://hackkosice.com/images/events/hk19/arrival_registration_hu9c5b0c793d9bb1618b8c596bf6a17dfb_3125095_640x360_fill_q60_box_smart1.jpg',
+      thumbImage:
+        'https://hackkosice.com/images/events/hk19/arrival_registration_hu9c5b0c793d9bb1618b8c596bf6a17dfb_3125095_640x360_fill_q60_box_smart1.jpg',
+  //    title: 'Image 3',
+      alt: 'Image 3',
+    },
+    {
+      image:
+        'https://hackkosice.com/images/events/hk19/arrival_registration_hu9c5b0c793d9bb1618b8c596bf6a17dfb_3125095_640x360_fill_q60_box_smart1.jpg',
+      thumbImage:
+        'https://hackkosice.com/images/events/hk19/arrival_registration_hu9c5b0c793d9bb1618b8c596bf6a17dfb_3125095_640x360_fill_q60_box_smart1.jpg',
+     // title: 'Image 4',
+      alt: 'Image 4',
+    },
+    {
+      image:
+        'https://hackkosice.com/images/events/hk19/arrival_registration_hu9c5b0c793d9bb1618b8c596bf6a17dfb_3125095_640x360_fill_q60_box_smart1.jpg',
+      thumbImage:
+        'https://hackkosice.com/images/events/hk19/arrival_registration_hu9c5b0c793d9bb1618b8c596bf6a17dfb_3125095_640x360_fill_q60_box_smart1.jpg',
+    // title: 'Image 5',
+      alt: 'Image 5',
+    },
+    {
+      image:
+        'https://hackkosice.com/images/events/hk19/arrival_registration_hu9c5b0c793d9bb1618b8c596bf6a17dfb_3125095_640x360_fill_q60_box_smart1.jpg',
+      thumbImage:
+        'https://hackkosice.com/images/events/hk19/arrival_registration_hu9c5b0c793d9bb1618b8c596bf6a17dfb_3125095_640x360_fill_q60_box_smart1.jpg',
+    //  title: 'Image 6',
+      alt: 'Image 6',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
